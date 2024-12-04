f5107078b04b1606af56b512ca6941f67ca52cea
Tatiana Snook <tatsnook@gmail.com>
2024-12-03 20:53:10 -0600

    initial commit

---
+++ src/main/java/org/launchcode/codingevents/controllers/EventController.java
@@ -0,0 +1,24 @@
+package org.launchcode.codingevents.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Controller
+@RequestMapping("events")
+public class EventController {
+
+    @GetMapping
+    public String event(Model model) {
+        List<String> events = new ArrayList<>();
+        events.add("Graduation");
+        events.add("Wedding");
+        events.add("Birthday");
+        model.addAttribute("events", events);
+        return "events/index";
+    }
+}
