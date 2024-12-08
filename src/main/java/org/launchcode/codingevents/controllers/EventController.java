f0a9c93e01219956f9aeb3b342d7ce18723adfee
Tatiana Snook <tatsnook@gmail.com>
2024-12-07 21:21:27 -0600

    add a form handler

--- src/main/java/org/launchcode/codingevents/controllers/EventController.java
+++ src/main/java/org/launchcode/codingevents/controllers/EventController.java
@@ -3,7 +3,9 @@ package org.launchcode.codingevents.controllers;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -12,12 +14,10 @@ import java.util.List;
 @RequestMapping("events")
 public class EventController {
 
+    private static List<String> events = new ArrayList<>();
+
     @GetMapping
     public String displayAllEvents(Model model) {
-        List<String> events = new ArrayList<>();
-        events.add("Graduation");
-        events.add("Wedding");
-        events.add("Birthday");
         model.addAttribute("events", events);
         return "events/index";
     }
@@ -26,5 +26,13 @@ public class EventController {
     @GetMapping("create")
     public String renderCreateEventForm() {
         return "events/create";
+    }
+
+    //lives at /events/create
+    @PostMapping("create")
+    public String createEvent(@RequestParam String eventName) {
+        events.add(eventName);
+        return "redirect:/events";
+
     }
 }
